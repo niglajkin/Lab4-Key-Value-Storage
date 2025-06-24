dec3dd74acfb7efdcd586ac8deaf2d8af87125c7
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-06-24 15:20:26 +0300

    Add app starting commands in Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello world!");
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ShardedKeyValueStore>();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.MapControllers();
+app.Run();
